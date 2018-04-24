2638d452386945d2d4f7ca75df814c7bbf3148f4
aloktiwari <alok.tiwari@hotmail.com>
2018-04-24 19:28:56 +0530

    notebook class created which provides a dictionary of notebook id and notebook name

---
+++ runmyzeppelin/notebook.py
@@ -0,0 +1,29 @@
+import requests
+
+class Notebook(object):
+    host = ''
+    port = ''
+    def __init__(self):
+        try:
+            f = open("/Users/aloktiwari/PycharmProjects/ZeppelinAutomationScript/zeppelin-server.properties" , "r")
+            inpHostPort = []
+            for line in f:
+                inpHostPort.append(line)
+            Notebook.host = inpHostPort[0].split("=")[1].strip()
+            Notebook.port = inpHostPort[1].split("=")[1]
+        except IOError:
+            print("file not found")
+
+    def getNotebookID(self):
+        testURL="http://%s:%s/api/notebook" % (Notebook.host,Notebook.port)
+        print(testURL)
+        r = requests.get(url=testURL)
+        data = r.json()
+        json_data = data['body']
+        subdict = {}
+        for item in json_data:
+            new_key=item['id']
+            new_value=item['name']
+            subdict[new_key]=new_value
+        return subdict
+
